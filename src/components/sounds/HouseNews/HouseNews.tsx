bd00dc7d6dd134fad66a70888d39017d4a30f29f
wezlar <deanpugh@deanpugh.com>
2023-09-23 16:30:35 +0100

    Adds in prettier and own eslint with configuration

--- src/components/sounds/HouseNews/HouseNews.tsx
+++ src/components/sounds/HouseNews/HouseNews.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
 const HouseNews = () => {
-
-  return     <div data-key="32" class="key">
-  <kbd>Space</kbd>
-  <span class="sound">Instrumental</span>
-</div>
+  return (
+    <div data-key="32">
+      <kbd>Space</kbd>
+      <span>Instrumental</span>
+    </div>
+  )
 }
 
-export default HouseNews;
+export default HouseNews
