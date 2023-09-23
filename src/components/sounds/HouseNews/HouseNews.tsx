9245ea8d45354866f28901b1c5b48117cbd5579a
wezlar <deanpugh@deanpugh.com>
2023-09-23 15:36:41 +0100

    Create-react-app with Typescript and Prettier installed

---
+++ src/components/sounds/HouseNews/HouseNews.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HouseNews = () => {
+
+  return     <div data-key="32" class="key">
+  <kbd>Space</kbd>
+  <span class="sound">Instrumental</span>
+</div>
+}
+
+export default HouseNews;
